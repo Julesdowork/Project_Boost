a4db45fc1375db044c6b6f26332ca81ffd242401
Julian McNeill <julesdowork@gmail.com>
2022-04-15 23:20:15 -0400

    Mathf.Sin() For Oscillation

--- Project_Boost/Assets/Scripts/Oscillator.cs
+++ Project_Boost/Assets/Scripts/Oscillator.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class Oscillator : MonoBehaviour
 {
+    const float tau = Mathf.PI * 2;     // ~6.283
+    float cycles;
+    Vector3 offset;
+    
     private Vector3 startingPos;
     [SerializeField] private Vector3 movementVector;
-    [SerializeField] [Range(0, 1)] private float movementFactor;
+    private float movementFactor;
+    [SerializeField] private float period = 2f;
 
     void Start()
     {
         startingPos = transform.position;
-        Debug.Log(startingPos);
     }
 
     void Update()
     {
-        Vector3 offset = movementVector * movementFactor;
+        cycles = Time.time / period;                    // continually growing over time
+        float rawSinWave = Mathf.Sin(cycles * tau);     // going from -1 to 1
+
+        movementFactor = (rawSinWave + 1f) / 2f;        // recalculated to go from 0 to 1 so its cleaner
+
+        offset = movementVector * movementFactor;
         transform.position = startingPos + offset;
     }
 }
